c9ea4e4bf2605d83a0d6f04eb3b4504d0f4400ea
AlanSunAlan <sunalansun@gmail.com>
2022-04-30 15:28:59 +0800

    Modify the batch size for training

--- centralized_train.py
+++ centralized_train.py
@@ -15,7 +15,7 @@ def parse_opt(known=False):
     parser.add_argument('--data', type=str, default= os.path.join(ROOT_DIR, 'dataset_yaml/VisDrone.yaml'), help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default= os.path.join(YOLO_DIR, 'data/hyps/hyp.scratch-low.yaml'), help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs, -1 for autobatch')
+    parser.add_argument('--batch-size', type=int, default=-1, help='total batch size for all GPUs, -1 for autobatch')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
     parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
